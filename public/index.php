28810d5bd9f6208791554e1ea0fce79990ad9e4d
Chris Rodgers <chris.rodgers@axion.com.au>
2019-01-25 13:49:33 +1100

    sends response

--- public/index.php
+++ public/index.php
@@ -3,7 +3,16 @@ declare(strict_types=1);
 
 use DI\ContainerBuilder;
 use ExampleApp\HelloWorld;
+use FastRoute\RouteCollector;
+use Middlewares\FastRoute;
+use Middlewares\RequestHandler;
+use Narrowspark\HttpEmitter\SapiEmitter;
+use Relay\Relay;
+use Zend\Diactoros\Response;
+use Zend\Diactoros\ServerRequestFactory;
 use function DI\create;
+use function DI\get;
+use function FastRoute\simpleDispatcher;
 
 require_once dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -12,10 +21,25 @@ $containerBuilder->useAutowiring(false);
 $containerBuilder->useAnnotations(false);
 $containerBuilder->addDefinitions([
   HelloWorld::class => create(HelloWorld::class)
+    ->constructor(get('Foo'), get('Response')),
+  'Foo' => 'bar',
+  'Response' => function() {
+    return new Response();
+  }
 ]);
 
 $container = $containerBuilder->build();
 
-$helloWorld = $container->get(HelloWorld::class);
-$helloWorld->announce();
+$routes = simpleDispatcher(function (RouteCollector $r) {
+  $r->get('/hello', HelloWorld::class);
+});
 
+$middlewareQueue = [];
+$middlewareQueue[] = new FastRoute($routes);
+$middlewareQueue[] = new RequestHandler($container);
+
+$requestHandler = new Relay($middlewareQueue);
+$response = $requestHandler->handle(ServerRequestFactory::fromGlobals());
+
+$emitter = new SapiEmitter();
+return $emitter->emit($response);
--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -3,10 +3,25 @@ declare(strict_types=1);
 
 namespace ExampleApp;
 
+use Psr\Http\Message\ResponseInterface;
+
 class HelloWorld
 {
-  public function announce(): void
+  private $foo;
+  private $response;
+
+  public function __construct(
+    string $foo,
+    ResponseInterface $response
+  ) {
+    $this->foo = $foo;
+    $this->response = $response;
+  }
+
+  public function __invoke(): ResponseInterface
   {
-    echo 'Hello, autoloaded world!';
+    $response = $this->response->withHeader('Content-Type', 'text/html');
+    $response->getBody()->write("<html><head></head><body>Hello, {$this->foo} world!</body></html>");
+    return $response;
   }
 }
